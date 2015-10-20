2db339983f762d89d00167ca826593a98e38aed2
styel07 <eleazarcorpuz@gmail.com>
2015-10-19 17:14:15 -1000

    updated dependedcies

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,17 @@
-gulp.task('connect', function(){
-  connect.server({
-    root: 'public',
-    livereload: true
-  });
-});
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
+  return gulp.src('./sass/styles.scss')
       .pipe(sass({
-        errLogToConsole: true,
         sourceComments: true,
         includePaths: ['bower_components/foundation/scss']
       }).on('error', sass.logError))
       .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('livereload', function (){
-  gulp.src('./public/**/*')
-  .pipe(connect.reload());
-});
-
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./public/**/*', ['livereload']);
 });
 
-gulp.task('default', ['connect', 'watch', 'sass']);
+gulp.task('default', ['sass','watch']);
